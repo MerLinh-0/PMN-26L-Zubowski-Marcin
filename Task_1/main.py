10d8df10c218394d60f79bc25f7a1570d31970b6
Marcin <marcin.zubowski@o2.pl>
2026-03-27 18:23:58 +0100

    Wczytywanie i wypisywanie danych z pliku iris.data

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,5 +1,15 @@
+import pandas as pd
+
+
+def load_data(path):
+    columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'species']
+    data = pd.read_csv(path, header=None, names=columns)
+    return data
+
+
 def main():
-    pass
+    data = load_data("iris.data")
+    print(data.iloc[:, :-1].values) # wszystkie wiersze i kolumny oprócz ostatniej
 
-
+    
 main()
